ba9ce496ad873db697a084c6669bf0d13af44ccc
Tony Joseph <Tonyj@emids.com>
2019-05-22 19:21:05 +0530

    My First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+
+import { EmployeeTitle } from './employee/employee-title.pipe';
+import { EmployeeCountComponent } from './employee/employee-count/employee-count.component';
+import { OthersComponent } from './others/others.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+    EmployeeTitle,
+    EmployeeCountComponent,
+    OthersComponent,
+    HomeComponent,
+    PageNotFoundComponent
+        
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
